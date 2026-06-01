4f82df69aa3476b193ef7f705f6806896d9f8554
Beforg <bruno.capsqn@gmail.com>
2026-06-01 11:10:27 -0300

    tf08-1: DTO e entidades para Registro Criado DTO: RegistroUsuarioDTO Criado entidade Aluno Criado entidade Professor Criado enum Role para roles do sistema Configurado SecurityConfiguration

--- src/main/java/unipampa/trueble/api/infra/SecurityConfiguration.java
+++ src/main/java/unipampa/trueble/api/infra/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package unipampa.trueble.api.infra;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,7 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
+    @Value("${api.version}") // Variável de ambiente para a base da API, por exemplo: "/api/v1"
+    private String api;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -21,11 +23,10 @@ public class SecurityConfiguration {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers(api+"/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
-                // O Spring Security agora lê a URL do properties e resolve toda a criptografia sozinho!
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
